8c229a21af1427aee977a78f9fd87e4c76cb65ca
Samanta Adriane Sedlaczek Luchoski <49082205+samantaluc@users.noreply.github.com>
2023-05-17 14:55:27 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -18,15 +18,14 @@ function setup() {
   ground.addImage("ground",groundImage);
   ground.x = ground.width /2;
   ground.velocityX = -4;
-  //criando invisible ground (chão invisível) 15/04
-  invisibleGround = createSprite(200,190,400,10); //cria o sprite 15/04
-  invisibleGround.visible = false; //a visibilidade é falsa, portanto não vai aparecer o sprite 15/04
+  //criando invisible ground (chão invisível) <<<<<<<<<<<<<<<
+  invisibleGround = createSprite(200,190,400,10); //cria o sprite <<<<<<<<<<<<<<<
+  invisibleGround.visible = false; //a visibilidade é falsa, portanto não vai aparecer o sprite <<<<<<<<<<<<<<<
 }
-
 function draw() {
   background(220);
   //pular quando barra de espaço é pressionada e adiciona o limite de que só pode pular entre a posiçao y 100 a 600
-  if (keyDown("space") && trex.y >= 100) { //15/04  adiciona o     && trex.y >= 100
+  if (keyDown("space") && trex.y >= 100) { //<<<<<<<<<<<<<<< adiciona o && trex.y >= 100
     trex.velocityY = -10;
   }
   trex.velocityY = trex.velocityY + 0.8
@@ -34,7 +33,6 @@ function draw() {
     ground.x = ground.width / 2;
   }
   //impedir trex de cair
-  trex.collide(invisibleGround); // 15/04 troca o ground por invisibleGround
-
+  trex.collide(invisibleGround); // <<<<<<<<<<<<<<< troca o ground por invisibleGround
   drawSprites();
 }
